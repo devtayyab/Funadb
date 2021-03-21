608faceb48cd0b64fb91f8c5ad193624a6e44837
muhammadtayyabgujjar <gujjartayyab9494@gmail.com>
2021-03-21 11:23:29 +0500

    funadb post data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from "react"
+import { useFormik, Formik, Form, ErrorMessage, Field } from 'formik';
+// import TextField from '@material-ui/core/TextField';
 
-import { Link } from 'gatsby';
+export default () =>{
+  const [mydata, setData] = useState("");
+  const formik = useFormik({
+    initialValues:{ 
+      name: '',
+      age: ''
+    },
+    onSubmit: (values)=>{
+      console.log(values);
+      fetch(`/.netlify/functions/hello`, {
+        method: 'post',
+        body: JSON.stringify(values)
+      })
+     .then(response => response.json())
+    .then(data => {
+      setData(data);
+      console.log("Data: " + JSON.stringify(data));
+    
+    });
+  }} 
+  
+  );
 
-export default () => {
-    return (
-        <div>
-          
-               
-                <p>This is my home page</p>
-                <Link to="/product/">Dynamically Created Page At Build Time</Link>
-           
-        </div>
-    );
-}
+  return (
+     <form onSubmit={formik.handleSubmit}>
+       {/* <div>tayyab+{mydata}</div> */}
+       <label htmlFor="Name">Name</label>
+       <input
+         id="name"
+         name="name"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.name}
+       />
+    <br></br>
+       <label htmlFor="lastName">age</label>
+       <input
+         id="age"
+         name="age"
+         type="number"
+         onChange={formik.handleChange}
+         value={formik.values.age}
+       />
+        <button type="submit">Submit</button>
+            </form>
+
+            )
+        }
